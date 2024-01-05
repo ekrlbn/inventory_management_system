9be649b6b85d860f92f93ffed4174e3626e0e63e
Ekrem <105484430+ekrlbn@users.noreply.github.com>
2024-01-05 21:49:44 +0300

    Add new functions and routes for purchases, profits, and product management

--- app/db.py
+++ app/db.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import configparser
+import datetime
 
 
 def get_db_connection():
@@ -57,3 +58,78 @@ def get_user_by_session_id(session_id):
     close_db_cursor(cursor)
     close_db_connection(connection)
     return user
+
+def get_all_purchases():
+    connection = get_db_connection()
+    cursor = get_db_cursor(connection)
+    cursor.execute("SELECT p. FROM purchase p LEFT JOIN purchase_detail pd ON p.id=pd.purchase_id")
+    purchases = cursor.fetchall()
+    close_db_cursor(cursor)
+    close_db_connection(connection)
+    return purchases
+
+def get_profit():
+    connection = get_db_connection()
+    cursor = get_db_cursor(connection)
+    cursor.execute("SELECT r.months month, r.revenue,e.expense, (r.revenue-e.expense) profit FROM "
+                   + "(SELECT MONTH(s.s_date) months, SUM(sd.quantity * sd.price) AS revenue FROM sale_detail sd LEFT JOIN sale s ON sd.sale_id=s.id GROUP BY MONTH(s.s_date) ORDER BY 1) r "
+                   + "LEFT JOIN " 
+                   + "(SELECT MONTH(p.p_date) months, SUM(pd.quantity * pd.price) AS expense FROM purchase_detail pd LEFT JOIN purchase p ON pd.purchase_id=p.id GROUP BY MONTH(p.p_date) ORDER BY 1) e "
+                   + "ON r.months=e.months;")
+    profits = cursor.fetchall()
+    close_db_cursor(cursor)
+    close_db_connection(connection)
+    profits = list(profits)
+    for i in range(len(profits)):
+        profits[i]["month"] = datetime.date(1900, profits[i]["month"], 1).strftime('%B')
+    return profits
+
+def delete_session():
+    connection = get_db_connection()
+    cursor = get_db_cursor(connection)
+    cursor.execute("UPDATE user SET session_id=NULL")
+    connection.commit()
+    close_db_cursor(cursor)
+    close_db_connection(connection)
+
+def get_suppliers():
+    connection = get_db_connection()
+    cursor = get_db_cursor(connection)
+    cursor.execute("SELECT * FROM supplier")
+    suppliers = cursor.fetchall()
+    close_db_cursor(cursor)
+    close_db_connection(connection)
+    return suppliers
+
+def add_product(name, quantity, price, supplier):
+    connection = get_db_connection()
+    cursor = get_db_cursor(connection)
+    cursor.execute("INSERT INTO product (name, quantity, current_price, supplier_id) VALUES (%s, %s, %s, %s)", (name, quantity, price, supplier))
+    connection.commit()
+    close_db_cursor(cursor)
+    close_db_connection(connection)
+
+def get_product(product_id):
+    connection = get_db_connection()
+    cursor = get_db_cursor(connection)
+    cursor.execute("SELECT * FROM product WHERE id=%s", (product_id,))
+    product = cursor.fetchone()
+    close_db_cursor(cursor)
+    close_db_connection(connection)
+    return product
+
+def update_product(product_id, name, quantity, price, supplier_id):
+    connection = get_db_connection()
+    cursor = get_db_cursor(connection)
+    cursor.execute("UPDATE product SET name=%s, quantity=%s, current_price=%s, supplier_id=%s WHERE id=%s", (name, quantity, price, supplier_id, product_id))
+    connection.commit()
+    close_db_cursor(cursor)
+    close_db_connection(connection)
+
+def delete_product(product_id):
+    connection = get_db_connection()
+    cursor = get_db_cursor(connection)
+    cursor.execute("DELETE FROM product WHERE id=%s", (product_id,))
+    connection.commit()
+    close_db_cursor(cursor)
+    close_db_connection(connection)
--- app/views.py
+++ app/views.py
@@ -45,7 +45,7 @@ def inventory():
     
     order_by = request.args.get("order_by")
     order = request.args.get("order")
-    if order_by is None or order_by not in ["name", "quantity", "price"]:
+    if order_by is None or order_by not in ["name", "quantity", "current_price"]:
         order_by = "name"
     if order is None or order not in ["asc", "desc"]:
         order = "asc"
@@ -53,3 +53,68 @@ def inventory():
     products = db.get_all_products( order_by=order_by, order=order)
     reverse_order = "desc" if order == "asc" else "asc"
     return render_template("inventory.html", products=products, reverse_order=reverse_order)
+
+@views.route("/purchases")
+def purchases():
+    if "session_id" not in session or not authorize_user(session["session_id"]):
+        return redirect(url_for("views.login"))
+    
+    purchases = db.get_all_purchases()
+    return render_template("purchases.html", purchases=purchases)
+
+@views.route("/profits")
+def revenue():
+    if "session_id" not in session or not authorize_user(session["session_id"]):
+        return redirect(url_for("views.login"))
+    
+    profits = db.get_profit()
+    return render_template("profits.html", profits=profits)
+
+@views.route("/logout")
+def logout():
+    session.pop("session_id", None)
+    db.delete_session()
+    return redirect(url_for("views.login"))
+
+@views.route("/new_product", methods=["POST"])
+def add_product():
+    if "session_id" not in session or not authorize_user(session["session_id"]):
+        return redirect(url_for("views.login"))
+    name = request.form.get("name")
+    quantity = request.form.get("quantity")
+    price = request.form.get("price")
+    supplier_id = request.form.get("supplier_id")
+    db.add_product(name, quantity, price, supplier_id)
+    return redirect(url_for("views.inventory"))
+
+@views.route("/new_product")
+def new_product():
+    if "session_id" not in session or not authorize_user(session["session_id"]):
+        return redirect(url_for("views.login"))
+    suppliers = db.get_suppliers()
+    return render_template("add_product.html", suppliers=suppliers)
+
+@views.route("/product/<int:product_id>")
+def product(product_id):
+    if "session_id" not in session or not authorize_user(session["session_id"]):
+        return redirect(url_for("views.login"))
+    product = db.get_product(product_id)
+    return render_template("product.html", product=product, suppliers=db.get_suppliers())
+
+@views.route("/product/<int:product_id>", methods=["POST"])
+def update_product(product_id):
+    if "session_id" not in session or not authorize_user(session["session_id"]):
+        return redirect(url_for("views.login"))
+    name = request.form.get("name")
+    quantity = request.form.get("quantity")
+    price = request.form.get("price")
+    supplier_id = request.form.get("supplier_id")
+    db.update_product(product_id, name, quantity, price, supplier_id)
+    return redirect(url_for("views.inventory"))
+
+@views.route("/product_delete/<int:product_id>", methods=["POST"])
+def delete_product(product_id):
+    if "session_id" not in session or not authorize_user(session["session_id"]):
+        return redirect(url_for("views.login"))
+    db.delete_product(product_id)
+    return redirect(url_for("views.inventory"))
